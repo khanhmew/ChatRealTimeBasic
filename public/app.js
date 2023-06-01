41927454c112c579e69650b15363f22870e874d4
khaphan-github <76431966+khaphan-github@users.noreply.github.com>
2023-06-01 19:08:37 +0700

    Demo ecdsa send message

--- public/app.js
+++ public/app.js
@@ -3,36 +3,18 @@ const msgText = document.querySelector('#msg')
 const btnSend = document.querySelector('#btn-send')
 const chatBox = document.querySelector('.chat-content')
 const displayMsg = document.querySelector('.message')
-
 let name;
-do{
+do {
     name = prompt('What is your name?')
-}while(!name)
-
-
+} while (!name)
 document.querySelector('#your-name').textContent = name
 msgText.focus()
 
-btnSend.addEventListener('click', (e)=>{
+btnSend.addEventListener('click', (e) => {
     e.preventDefault()
     sendMsg(msgText.value)
-    msgText.value="";
+    msgText.value = "";
     msgText.focus()
-})
-
-const sendMsg = message => {
-    let msg = {
-        user: name,
-        message: message.trim()
-    }
-    display(msg, 'you-message')
-    chatBox.scrollTop = chatBox.scrollHeight;
-    socket.emit('sendMessage', msg)
-}
-
-socket.on('sendToAll', msg =>{
-    display(msg, 'other-message')
-    chatBox.scrollTop = chatBox.scrollHeight;
 })
 
 const display = (msg, type) => {
@@ -55,3 +37,55 @@ const display = (msg, type) => {
     msgDiv.innerHTML = innerText;
     displayMsg.appendChild(msgDiv)
 }
+
+// ===============================================================
+// Generate ECDSA key pair
+const ec = new elliptic.ec('secp256k1');
+const keyPair = ec.genKeyPair();
+
+const publicKey = keyPair.getPublic('hex');
+const privateKey = keyPair.getPrivate('hex');
+
+
+let isSendKey = false;
+const sendMsg = message => {
+    const signature = keyPair.sign(message).toDER('hex');
+
+    let msg = {
+        user: name,
+        message: message.trim(),
+        signature: signature
+    }
+    display(msg, 'you-message')
+
+    if (!isSendKey) {
+        socket.emit('sendPublicKey', publicKey);
+        isSendKey = true;
+    }
+
+    socket.emit('sendMessage', msg);
+
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+// when recieve message
+socket.on('chat-chanel', msg => {
+    display(msg, 'other-message');
+    const keyPairB = ec.keyFromPublic(localStorage.getItem('publicKey'), 'hex');
+
+    const isSignatureValid = keyPairB.verify(msg.message, msg.signature);
+
+    if (isSignatureValid) {
+        console.log("Message hợp lệ");
+    }
+    else {
+        console.log("Message không hợp lệ");
+
+    }
+    chatBox.scrollTop = chatBox.scrollHeight;
+})
+
+socket.on('key-chanel', key => {
+    console.log("Key receive", key);
+    localStorage.setItem('publicKey', key);
+})
--- server.js
+++ server.js
@@ -1,25 +1,19 @@
-const { Socket } = require('dgram');
 const express = require('express')
 const path = require('path');
-
 const app = express()
 const http = require('http').createServer(app);
-
-
 app.use(express.static(path.join(__dirname, 'public')))
 
 const io = require('socket.io')(http)
-io.on('connection', socket =>{
-    console.log('Connected Ready');
-
+io.on('connection', socket => {
     socket.on('sendMessage', msg => {
-        socket.broadcast.emit('sendToAll', msg)
-    })
+        socket.broadcast.emit('chat-chanel', msg)
+    });
+    socket.on('sendPublicKey', msg => {
+        socket.broadcast.emit('key-chanel', msg)
+    });
 })
 
-
-
-const PORT = process.env.PORT || 3000
-http.listen(PORT, ()=>{
-    console.log('Server is running on port', PORT)
+http.listen(3000, () => {
+    console.log('Server is running on port 3000')
 })
